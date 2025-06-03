640d3979c1a1d48ee17e826187ffb3f02a7f2de5
Dabhi Amit <amitdabhi526@gmail.com>
2025-06-03 12:19:14 +0530

    js

---
+++ 01_intro_js/index.js
@@ -0,0 +1,54 @@
+console.log("Hello word")
+
+console.log(10 + 20)
+
+let n = 10
+let n1 = 10.5
+
+n1 = "float"
+
+console.log(n + n1)
+
+let arr = [1, "amit", 2.5, true]
+
+console.log(arr)
+
+let obj = {
+    name:"amul milk",
+    price:20,
+    rate:4.7
+}
+
+let obj2 = {}
+
+obj2.name ="jay";
+obj2.age = 22;
+obj2.mark = 80;
+
+console.log(obj)
+
+console.log(obj.price)
+
+
+document.write("<b>Name: </b>" + obj.name + "<br/>")
+document.write("<b>price: </b>" + obj.price + "<br/>")
+document.write("<b>rate: </b>" + obj.rate + "<br/><hr/>")
+
+document.write("<b>Name: </b>" + obj2.name + "<br/>")
+document.write("<b>age: </b>" + obj2.age + "<br/>")
+document.write("<b>mark: </b>" + obj2.mark + "<br/><hr/>")
+
+console.table(arr)
+
+console.table(obj)
+
+console.table(obj2)
+
+// number
+// string
+// bolean
+// undefine
+// null
+
+// arr
+// object
