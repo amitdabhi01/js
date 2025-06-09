d899054d8c74ea938c291388d4cbd33a30c72a8d
Dabhi Amit <amitdabhi526@gmail.com>
2025-06-09 13:21:19 +0530

    Object

---
+++ 06_object/index.js
@@ -0,0 +1,118 @@
+
+// let obj = new Object()
+// let obj = {}
+
+// obj.title = "amul milk"
+// obj.price = 34
+// obj.rate = 4.8
+
+// console.log(obj)
+// console.log(obj.title)
+
+// obj.price = 43
+
+// console.log(obj)
+
+// what is object = instance of the class, in wich we can store the data in key or value pairs 
+
+
+
+// let data = {
+//     org:"Ketch",
+//     found: 2009,
+//     founder: "pratik Shukla",
+//     "board Member": "aman vats"                                                                                       
+// }
+// Object.freeze(data)
+
+// data.found = 2000
+// data.revenue = 2000000
+
+// console.log(data)
+
+
+
+// let newObj = Object.assign (data, obj)
+
+// console.log(newObj)
+
+// console.log(newObj.org)
+// console.log(newObj["board Member"])
+// console.log(newObj["founder"])
+
+// let keys = Object.keys(newObj)
+// let values = Object.values(newObj)
+
+// console.log(keys)
+// console.log(values)
+
+
+// for(let x in newObj){
+//     // console.log(x)
+//     console.log(x + " : " + newObj[x])
+// }
+
+// console.table(newObj)
+
+
+// let a = 200;
+// let b = a;
+
+// b = 300;
+
+// console.log(a)
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+// NESTED OBJECT
+
+// let obj1 = new Object()
+// let obj1 = {
+//     title: "amul milk",
+//     price: 34,
+//     rate: 4.8
+// }
+
+
+// let obj2 = new Object()
+// let obj2 = {}
+
+// obj2.title = obj1.title
+// obj2.price = obj1.price
+// obj2.rate = obj1.rate
+
+// obj2.title = "Natural milk"
+
+// console.log(obj1)
+// console.log(obj2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// NESTED OBJECT
+
+// let obj1 = new Object()
+let obj1 = {
+    title: "amul milk",
+    price: 34,
+    rate: 4.8
+}
