ef4df6e5b701a27d865e7cfe1ad795a193226879
Dabhi Amit <amitdabhi526@gmail.com>
2025-06-10 13:30:51 +0530

    array

---
+++ 07_array/index.js
@@ -0,0 +1,77 @@
+
+// let arr = new Array()
+
+// let arr = []
+
+// console.log(arr)
+
+// arr[0] = 10
+// arr[1] = 20
+
+// arr.push("Amit")
+
+// arr[5] = 30
+
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+// console.log(arr)
+// console.log(arr.length)
+
+
+// let nums = [22, 33, 11, 65, 78, 34]
+
+// nums.shift()
+// nums.unshift("99")
+// console.log(nums)
+
+// console.log(nums.indexOf(99))
+// console.log(nums.at(nums.length-1))
+// console.log(nums.includes(78))
+
+
+// let check = nums.every(odd)
+
+// let check = nums.every(function odd(ele){
+//     return ele%2 == 0
+// })
+
+
+// let check = nums.every(function(ele){
+//     return ele%2 == 0
+// })
+
+// let check = nums.every((ele) => {
+//     return ele%2 == 0
+// })
+
+// let check = nums.every((ele) => ele%2 == 0)
+
+// console.log(check)
+
+
+// function odd(ele){
+//     return ele%2 == 0
+// }
+
+// const odd = function(ele){
+//     return ele%2 == 0
+// }
+
+
+// console.log(nums.every())
+
+// let check = nums.every((ele) => ele%2 == 0)
+// let check = nums.every((ele) => ele > 22)
+
+// console.log("odd: " + check)
+
+
+// let evNum = nums.find(function (n){
+//     return n%2 == 0
+// })
+
+// let evNum = nums.find((n) => n%2 == 0)
+
+// console.log(evNum)
---
+++ 07_array/slice.js
@@ -0,0 +1,28 @@
+
+let data = ["one", "two", "three", "four", "five", 11, 22]
+
+// slice(start_index, end_index)
+
+// let chunk = data.slice(0, 4)
+// let revchunk = data.slice(-3, 7)
+
+// console.log(chunk)
+// console.log(revchunk)
+
+
+// console.log(chunk)
+// console.log(data)
+// console.log(revChunk)
+
+
+
+
+// splice(start_index, Node. of ele)
+
+// let cut = data.splice(0, 4)
+
+let revcut = data.splice(-5, 5)
+
+// console.log(cut)
+console.log(revcut)
+console.log(data)
