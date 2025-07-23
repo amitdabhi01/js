2f362ac204ab1f0c343124b23d597813f4a099ae
Dabhi Amit <amitdabhi526@gmail.com>
2025-07-23 11:59:34 +0530

    calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,6 +1,24 @@
 
-let display = document.querySelector("#display")
+const display = document.querySelector("#display")
 
-function click(a){
-    display.value=a
+function append(e){
+    display.value += e
+}
+
+function clearDisplay(){
+    display.value = " "
+}
+
+function deleteEle(){
+    if(display.value > 0){
+       display.value = display.value.slice(0, -1)
+    }
+}
+
+function calculate(){
+    display.value = eval(display.value)
+}
+
+function percentage(){
+    display.value = display.value / 100
 }
