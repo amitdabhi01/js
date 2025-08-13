3304c36fc63b7757c7146d2ea55e44a79df0ebdb
Dabhi Amit <amitdabhi526@gmail.com>
2025-08-13 13:04:28 +0530

    array repeat

---
+++ RV_Array/array.js
@@ -0,0 +1,10 @@
+// an array in javascript is a data structure used to store multiple values in a single variable. it can hold various data types and allows for dynamic resizing. elements are accessed by their index, starting from 0
+
+
+// creating array using array litrerals
+
+let cars = ["BMW", "MERCEDES", "SKODA", "WOLKSWAGAN", "AUDI"]
+
+console.log("cars name", cars)
+
+console.log(typeof cars)
---
+++ RV_Array/array1.js
@@ -0,0 +1,31 @@
+// accessing elements of an array
+
+let fruits = ["apple", "banana", "guava", "cherry", "orange"]
+
+console.log("Fruits name", fruits)
+
+
+// Any element in the array can be accessed using the index number. The index in the arrays starts with 0.
+
+console.log("Favorite fruits is", fruits[3])
+
+
+// accessing first Element of an array
+
+console.log("first fruits is ",fruits[0])
+
+
+// accessing last element of an array
+
+console.log("last fruits is", fruits[fruits.length - 1])
+
+
+//  Modifying the Array Elements
+
+// fruits array before changing elements
+
+console.log("before in 2 index ele is", fruits[2])
+
+fruits[2] = "strawbarry"
+
+console.log("fruits", fruits)
