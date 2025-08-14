5eb7ed890f6284719cf12188d5c9a1da571c9a48
Dabhi Amit <amitdabhi526@gmail.com>
2025-08-14 13:01:45 +0530

    array

---
+++ RV_Array/array3.js
@@ -0,0 +1,37 @@
+// calculating array length
+
+const fruits = [
+  "apple",
+  "banana",
+  "cheery",
+  "mango",
+  "watermelon",
+  "grapes",
+  "strawberry",
+];
+
+console.log(fruits.length, fruits)
+
+// Increase and Decrease the Array Length
+
+console.log("fruits elements and their length before", fruits.length, fruits)
+
+// increas length
+
+fruits.length = 10
+
+console.log(
+  "fruits element and its length after increasing: ",
+  fruits.length,
+  fruits
+);
+
+// decrease length
+
+fruits.length = 4
+
+console.loog(
+    "fruits elent and its lentgh after decreeing",
+    fruits.length,
+    fruits
+)
