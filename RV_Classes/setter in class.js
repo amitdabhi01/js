f6cbd06c0de88eeeb98f82e65cd716127c1c3bbd
Dabhi Amit <amitdabhi526@gmail.com>
2025-08-22 13:39:50 +0530

    setter in class

---
+++ RV_Classes/setter in class.js
@@ -0,0 +1,21 @@
+// using setter we can set the properties of class object
+
+// example
+
+class Person{
+    constructor(name, age, job){
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    set personJob(jobname){
+        return (this.job = jobname)
+    }
+}
+
+const personDetails = new Person("john", 22);
+
+personDetails.personJob = "software engineer";
+
+console.log("personDetails :", "my name is ", personDetails.name + " " + "my age is", personDetails.age + " " + "and i am working as " + personDetails.job);
