9dc452e0cf4da92313e96cb11b254db733a73b76
Dabhi Amit <amitdabhi526@gmail.com>
2025-08-26 13:17:30 +0530

    callback,  set time out

---
+++ RV_asynchronous in js/callback.js
@@ -0,0 +1,21 @@
+// Where callbacks really shine are in asynchronous functions, where one function has to wait for another function (like waiting for a file to load).
+
+// if you want to wait for the result of the previous function call before the next statement is executed, you can use a callback function.
+
+// callback is function passed as an argument to another function
+// using callback we can do asynchronous task in js
+// a callback function it runs after another function has finished
+// when you pass functions as an arguments don't use parenthesis
+
+// callback example
+
+function callbackFun(result){
+    console.log("The totle of sum is: ", result)
+}
+
+function totalSumOfNum(a, b, callbackFun){
+    let result = a + b;
+    callbackFun(result)
+}
+
+totalSumOfNum(10, 20, callbackFun)
---
+++ RV_asynchronous in js/setTimeOut.js
@@ -0,0 +1,32 @@
+// The setTimeout() method executes a block of code after the specified time. The method executes the code only once.
+
+// The commonly used syntax of JavaScript setTimeout is:
+
+// setTimeout(function, milliseconds);
+// Its parameters are:
+
+// function - a function containing a block of code
+// milliseconds - the time after which the function is executed
+// The setTimeout() method returns an intervalID, which is a positive integer.
+
+const greeting = () => {
+    console.log("greeting function Output will print hello after 3 seconds")
+    setTimeout(() => {
+        console.log("namaste duniya")
+    }, 3000)
+}
+
+greeting()
+
+// another example
+
+const currentYear = () => {
+    console.log("checking current Year");
+
+    setTimeout(() => {
+        let currentYear = new Date().getFullYear;
+        console.log("current year: ", currentYear)
+    }, 5000)
+}
+
+currentYear()
