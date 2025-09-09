29ee0b82dfad3a5974385a93f87226d36fb477a1
Dabhi Amit <amitdabhi526@gmail.com>
2025-09-09 12:03:39 +0530

    j-query, selectors

---
+++ RV_jquery/selectors/script.js
@@ -0,0 +1,13 @@
+// $("*").css("background-color","gray")
+
+$("div>p").css("color", "red")
+
+$("ul>li").css("color","blue")
+
+$(".div").css("color", "green")
+
+$("[href]").css({
+    "color":"pink",
+    "text-decoration":"none",
+    "font-size":"30px",
+})
