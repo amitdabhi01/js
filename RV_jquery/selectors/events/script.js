a49781ffe2e66033c96461b271e24f49f4b61d0f
Dabhi Amit <amitdabhi526@gmail.com>
2025-09-10 12:33:32 +0530

    on mouse enter and on mouse leave

--- RV_jquery/selectors/events/script.js
+++ RV_jquery/selectors/events/script.js
@@ -19,3 +19,36 @@
 // $("document").ready(()=> {
 //     $("#btn").click(handleClick)
 // })
+
+// double click
+
+// $("document").ready(()=>{
+//     $("#btn").dblclick(()=>{
+//         $("#img").css({
+//             width : "500",
+//             height : "400",
+//         })
+//     })
+// })
+
+// on mouse enter 
+
+$("document").ready(()=>{
+    $("#img").mouseenter(()=>{
+        $("#img").css({
+            width : "500",
+            height : "400",
+        })
+    })
+})
+
+// on mouse leave
+
+$("document").ready(() => {
+    $("#img").mouseleave(()=>{
+        $("#img").css({
+            width : "400",
+            height : "300",
+        })
+    })
+})
