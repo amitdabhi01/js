87d4d9c3fdf1f172a03bc25090253ed2752febb1
Dabhi Amit <amitdabhi526@gmail.com>
2025-09-10 12:23:18 +0530

    click event

---
+++ RV_jquery/selectors/events/script.js
@@ -0,0 +1,21 @@
+// $("document").ready(()=>{
+//     $("#btn").click(()=>{
+//         $("#img").css({
+//             width : "500",
+//             height : "400",
+//         })
+//     })
+// })
+
+// using different method 
+
+// function handleClick(){
+//     $("#img").css({
+//             width : "500",
+//             height : "400",
+//         })
+// }
+
+// $("document").ready(()=> {
+//     $("#btn").click(handleClick)
+// })
