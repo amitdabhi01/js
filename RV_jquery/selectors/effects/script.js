ddc1feec9866cf0294e43ce78d66a8d7eb908d9c
Dabhi Amit <amitdabhi526@gmail.com>
2025-09-11 11:33:28 +0530

    hide, show, toggle

---
+++ RV_jquery/selectors/effects/script.js
@@ -0,0 +1,17 @@
+$("document").ready(() => {
+    $("#hide").click(() => {
+        $("#img").hide()
+    })
+
+    $("#show").click(() => {
+        $("#img").show()
+    })
+
+    $("#toggle").click(() => {
+        $("#img").toggle()
+    })
+
+    // fade effects
+
+    
+})
