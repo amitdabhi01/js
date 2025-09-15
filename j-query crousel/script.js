60f14243b82bc917b161281e2cfef42d40b7f763
Dabhi Amit <amitdabhi526@gmail.com>
2025-09-15 12:18:32 +0530

    j-query carousel

---
+++ j-query crousel/script.js
@@ -0,0 +1,26 @@
+$("document").ready(() => {
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        900:{
+            items:2
+        },
+        1000:{
+            items:2
+        },
+        1400:{
+            items:2
+        },
+    }
+})
+})
